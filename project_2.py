4b70db0cda3bbee8579e88070c0f15a76df02fa1
Aydın Kabataş <aydin.kabatas@hotmail.com>
2022-07-18 22:47:32 +0300

    Güncel Çalışma (Beyza Edit)

--- project_2.py
+++ project_2.py
@@ -23,7 +23,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./NetflixOriginals.csv',encoding="ANSI")
+df = pd.read_csv('./NetflixOriginals.csv',encoding="ISO-8859-1")
 
 #Sütunlardaki boşlukları alt tire ile değiştirdim
 df.columns = df.columns.str.replace(" ","_")
@@ -37,30 +37,27 @@ print(corra)
 sns.regplot(x=df['IMDB_Score'], y=df['Runtime'])
 plt.show()
 
-"""
+
 #IMDB Puanı en yüksek olan ilk 10 'Genre' hangileridir? Görselleştiriniz.
-df.groupby("Genre").agg({"IMDB Score": "max"}).sort_values(by="IMDB Score", ascending=False)[0:10]
-genrescore_on = df.groupby("Genre").agg({"IMDB Score": "max"}).sort_values(by="IMDB Score", ascending=False)[
-                0:10].reset_index()
+df.groupby("Genre").agg({"IMDB_Score": "max"}).sort_values(by="IMDB_Score", ascending=False)[0:10].reset_index()
+genrescore_on = df.groupby("Genre").agg({"IMDB_Score": "max"}).sort_values(by="IMDB_Score", ascending=False)[0:10].reset_index()
 
 
-print (df.groupby("Genre").agg({"IMDB Score": "max"}).sort_values(by="IMDB Score", ascending=False)[0:10])
-sns.lineplot(y=genrescore_on["Genre"], x=genrescore_on["IMDB Score"])
+print (df.groupby("Genre").agg({"IMDB_Score": "max"}).sort_values(by="IMDB_Score", ascending=False)[0:10])
+sns.lineplot(y=genrescore_on["Genre"], x=genrescore_on["IMDB_Score"])
 plt.show()
-"""
 
-"""
 #Veri setinde bulunan filmlerde en çok kullanılan 3 dili bulunuz.
-data['Language'].value_counts().head(3)
-"""
+df['Language'].value_counts().head(3)
 
-"""
-#Hangi dilde yayımlanan filmler en düşük ortalama IMBD puanına sahiptir? Görselleştiriniz.(Görselleştirmesinden emin değilim)
-imdb_most_low = df[df['IMDB Score']<5][['Language']].sort_values('IMDB Score', ascending = True)
 
-fig = px.bar(imdb_most_low, x='Language', y= 'IMDB Score', color = 'Title')
-fig.show()
-"""
+
+# #Hangi dilde yayımlanan filmler en düşük ortalama IMBD puanına sahiptir? Görselleştiriniz.(Görselleştirmesinden emin değilim)
+# imdb_most_low = df[df['IMDB_Score']<5][['Language']].sort_values('IMDB_Score', ascending = True)
+
+# fig = px.bar(imdb_most_low, x='Language', y= 'IMDB_Score', color = 'Title')
+# fig.show()
+
 
 """ 
 #IMDP Puanı yüksek olan 10 film.
