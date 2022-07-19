57a02ea95094de28fbc919040246077dd04d8427
fatihtuzunn <fatihtuzun11@gmail.com>
2022-07-19 11:01:07 +0300

    update

--- project_2.py
+++ project_2.py
@@ -2,21 +2,23 @@
 
 Veri setine göre uzun soluklu filmler hangi dilde oluşturulmuştur? Görselleştirme yapınız.
 2019 Ocak ile 2020 Haziran tarihleri arasında 'Documentary' türünde çekilmiş filmlerin IMDB değerlerini bulup görselleştiriniz.
-İngilizce çekilen filmler içerisinde hangi tür en yüksek IMDB puanına sahiptir?
+[Fatih] İngilizce çekilen filmler içerisinde hangi tür en yüksek IMDB puanına sahiptir?
 'Hindi' Dilinde çekilmiş olan filmlerin ortalama 'runtime' suresi nedir?
 'Genre' Sütunu kaç kategoriye sahiptir ve bu kategoriler nelerdir? Görselleştirerek ifade ediniz.
-Veri setinde bulunan filmlerde en çok kullanılan 3 dili bulunuz.
-IMDB puanı en yüksek olan ilk 10 film hangileridir?
-IMDB puanı ile 'Runtime' arasında nasıl bir korelasyon vardır? İnceleyip görselleştiriniz.
-IMDB Puanı en yüksek olan ilk 10 'Genre' hangileridir? Görselleştiriniz.
-'Runtime' değeri en yüksek olan ilk 10 film hangileridir? Görselleştiriniz.
-Hangi yılda en fazla film yayımlanmıştır? Görselleştiriniz.
-Hangi dilde yayımlanan filmler en düşük ortalama IMBD puanına sahiptir? Görselleştiriniz.
-Hangi yılın toplam "runtime" süresi en fazladır?
-Her bir dilin en fazla kullanıldığı "Genre" nedir?
-Veri setinde outlier veri var mıdır? Açıklayınız.
+[Beyza] Veri setinde bulunan filmlerde en çok kullanılan 3 dili bulunuz.
+[Fatih] IMDB puanı en yüksek olan ilk 10 film hangileridir?
+[Fatih] IMDB puanı ile 'Runtime' arasında nasıl bir korelasyon vardır? İnceleyip görselleştiriniz.
+[Beyza] IMDB Puanı en yüksek olan ilk 10 'Genre' hangileridir? Görselleştiriniz.
+[Fatih] 'Runtime' değeri en yüksek olan ilk 10 film hangileridir? Görselleştiriniz.
+[Şüheda] Hangi yılda en fazla film yayımlanmıştır? Görselleştiriniz.
+[Beyza] Hangi dilde yayımlanan filmler en düşük ortalama IMBD puanına sahiptir? Görselleştiriniz.
+[Şüheda] Hangi yılın toplam "runtime" süresi en fazladır?
+[Aydın] Her bir dilin en fazla kullanıldığı "Genre" nedir?
+[Aydın] Veri setinde outlier veri var mıdır? Açıklayınız
 
 """
+
+
 
 
 import pandas as pd
@@ -29,6 +31,9 @@ df = pd.read_csv('./NetflixOriginals.csv',encoding="ISO-8859-1")
 df.columns = df.columns.str.replace(" ","_")
 
 
+
+"""
+
 #IMDB puanı ile 'Runtime' arasında nasıl bir korelasyon vardır? İnceleyip görselleştiriniz.
 
 corra= df['IMDB_Score'].corr(df['Runtime'])
@@ -37,8 +42,18 @@ print(corra)
 sns.regplot(x=df['IMDB_Score'], y=df['Runtime'])
 plt.show()
 
+"""
+
+
+
+
+
+
+
+"""
 
 #IMDB Puanı en yüksek olan ilk 10 'Genre' hangileridir? Görselleştiriniz.
+
 df.groupby("Genre").agg({"IMDB_Score": "max"}).sort_values(by="IMDB_Score", ascending=False)[0:10].reset_index()
 genrescore_on = df.groupby("Genre").agg({"IMDB_Score": "max"}).sort_values(by="IMDB_Score", ascending=False)[0:10].reset_index()
 
@@ -50,17 +65,49 @@ plt.show()
 #Veri setinde bulunan filmlerde en çok kullanılan 3 dili bulunuz.
 df['Language'].value_counts().head(3)
 
+"""
 
 
-# #Hangi dilde yayımlanan filmler en düşük ortalama IMBD puanına sahiptir? Görselleştiriniz.(Görselleştirmesinden emin değilim)
-# imdb_most_low = df[df['IMDB_Score']<5][['Language']].sort_values('IMDB_Score', ascending = True)
 
-# fig = px.bar(imdb_most_low, x='Language', y= 'IMDB_Score', color = 'Title')
-# fig.show()
+
+
+
+
+"""
+#Hangi dilde yayımlanan filmler en düşük ortalama IMBD puanına sahiptir? Görselleştiriniz.(Görselleştirmesinden emin değilim)
+
+
+#Düzenlemeler yapıldı son hali kod sahibinin insiyatifinde.(Fatih)
+
+
+imdb_most_low = df[df['IMDB_Score']<5][["Language", "IMDB_Score"] ].sort_values('IMDB_Score', ascending = True)
+
+print(imdb_most_low)
+
+
+#Plotly kütüphanesi kullanmak amaçlanmış sanırım. (Fatih)
+fig = px.bar(imdb_most_low, x='Language', y= 'IMDB_Score', color = 'Title')
+fig.show()
+
+
+#Alternatif olarak. (Fatih)
+sns.barplot(x='Language', y='IMDB_Score', data = imdb_most_low)
+plt.show()
+
+"""
+
+
+
+
+
+
+
 
 
 """ 
 #IMDP Puanı yüksek olan 10 film.
+
+
 df.sort_values(by="IMDB_Score", ascending = False, inplace = True)
 df = df.reset_index()
 print(df.loc[:10]) 
@@ -68,22 +115,67 @@ print(df.loc[:10])
 """
 
 
-""" df.sort_values(by="Runtime", ascending = False, inplace = True)
-df = df.reset_index()
-print(df.head(10)) 
- """
+
+
+
+
+
+"""
+
+#'Runtime' değeri en yüksek olan ilk 10 film hangileridir? Görselleştiriniz.
+
+df.sort_values(by="Runtime", ascending = False, inplace = True)
+data = df.reset_index().head(10)
+print(data) 
+
+sns.violinplot(
+    x='Runtime',
+    y='Title',
+    data =df.nlargest(10, 'Runtime')
+)
+
+plt.show()
+
+"""
+
+
+
+
+
+
+
+
+"""
+
+#'Genre' Sütunu kaç kategoriye sahiptir ve bu kategoriler nelerdir? Görselleştirerek ifade ediniz.
+(TAMAMLANMADI [BOŞTA])
+
+
+data = df["Genre"].unique()
+
+print(data)
+
+#set(df.Genre)
+
+"""
+
+
+
+
+
+
+
 
 """
 İngilizce çekilen filmler içerisinde hangi tür en yüksek IMDB puanına sahiptir?
+(TAMAMLANMADI [Fatih] )
 
 engMovies = df[df["Language"] == "English"]
 
 print(engMovies["IMDB_Score"].idxmax())
 
-"""
+#-------------------------------
 
-
-"""
 #print(engMovies["IMDB_Score"].median())   6.4
 
 
@@ -91,9 +183,12 @@ result = engMovies[engMovies["IMDB_Score"] > 6.4]
 print(result["Genre"].mode())
 
 
--------------------
 
- #satır sayısı, sütun sayısı gösterir
+-------------------
+BİR TAKIM BİLGİLER
+
+
+#satır sayısı, sütun sayısı gösterir
 print(df.shape)
 
 #sütunların data typlearına bakıyorum
